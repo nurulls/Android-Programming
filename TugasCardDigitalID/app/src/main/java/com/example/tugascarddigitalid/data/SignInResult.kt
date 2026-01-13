6216717e883d3c6f338ecc3825b0379da21bd316
nurul siti khodijah <nurulsitikhodijah04@gmail.com>
2026-01-13 16:27:43 +0800

    Pertemuan 10

---
+++ TugasCardDigitalID/app/src/main/java/com/example/tugascarddigitalid/data/SignInResult.kt
@@ -0,0 +1,12 @@
+package com.example.tugascarddigitalid.data
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?
+)
